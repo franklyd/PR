6293f5e1594b66a26afac10a8165275b99ff5587
Septian Gilang Permana Putra <septiangilang92@gmail.com>
2017-01-29 12:46:53 +0100

    Update handwritten function

--- handwritten.m
+++ handwritten.m
@@ -5,12 +5,13 @@ obj = prnist([0:9],[1:10]);
 lab=getlabels(obj);
 
 % load image and assign labels
-Imgs = dir(['hw' '/' '*.jpg']);
+Imgs = dir(['hw' '/' '*.bmp']);
 NumImgs = size(Imgs,1);
 image_set = [];
 for i=1:NumImgs
 im = imread(['hw' '/' Imgs(i).name]);
-imbw = im2bw(im(:,:,1));
+imcomp = imcomplement(im);
+imbw = im2bw(imcomp(:,:,1));
 imobj = im2obj(imbw)*im_resize([],[32,32]);
 imlab =  setlabels(imobj, lab(i,:));
 image_set=[image_set; imlab];
